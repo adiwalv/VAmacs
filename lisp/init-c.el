3e3513216746f03e873bc075344de58266801d63
Vikas Adiwal <adiwalv@gmail.com>
2018-12-26 21:10:33 +0530

    added c header autocomplete

--- lisp/init-c.el
+++ lisp/init-c.el
@@ -61,6 +61,16 @@
         :functions company-backend-with-yas
         :init (cl-pushnew (company-backend-with-yas 'company-c-headers) company-backends)))))
 
+(use-package auto-complete-c-headers)
+
+(defun my:ac-c-headers-init ()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers))
+
+(add-hook 'c++-mode-hook 'my:ac-c-headers-init)
+(add-hook 'c-mode-hook 'my:ac-c-headers-init)
+
+
 (provide 'init-c)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
