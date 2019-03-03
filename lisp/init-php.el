9bb8740d0b0be241def89c4f1fc3a5cfe048fe66
Vikas Adiwal <adiwalv@gmail.com>
2019-03-03 20:21:01 +0530

    updated init-php

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -8,6 +8,8 @@
 ;;; Code:
 
 
+(use-package ac-php)
+
 (unless (package-installed-p 'ac-php )
   (package-refresh-contents)
   (package-install 'ac-php )
@@ -25,7 +27,16 @@
              (define-key php-mode-map  (kbd "C-]") 'ac-php-find-symbol-at-point)   ;goto define
              (define-key php-mode-map  (kbd "C-t") 'ac-php-location-stack-back)    ;go back
              ))
+(use-package company-php)
 (add-hook 'php-mode-hook
+          '(lambda ()
+             (require 'company-php)
+             (company-mode t)
+             (ac-php-core-eldoc-setup) ;; enable eldoc
+             (make-local-variable 'company-backends)
+             (add-to-list 'company-backends 'company-ac-php-backend)))
+  
+  (add-hook 'php-mode-hook
           '(lambda ()
              (auto-complete-mode t)
              (require 'ac-php)
