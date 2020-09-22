1026cd107c91300994a470a5fcc1a757c2d1aa5a
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-22 16:48:58 +0530

    removed milkbox as it was hitting 4040

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -54,7 +54,7 @@
 
 (set-package-archives va-package-archives)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+;;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
